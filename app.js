09e4bffb0c98454fdb648c4aa083b57119556da4
Collins Kariuki <collinsmunkary@gmail.com>
2023-02-13 10:00:27 +0300

    Parsing the request body

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-const http = require('http');
-
 //import express
 const express = require('express');
+const bodyParser = require('body-parser'); //import after installation
 
 //create express application, initializes a new app
 const app = express();
 
-app.use((req,res,next) => {
-    console.log('In the middleware!');
-    next(); //Allows the request to continue to the next middleware in line
+//body parser - done before routing as each request should
+//be parsed no matter where it ends up
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next) => {
+    res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>")
 })
 
-app.use((req,res,next) => {
-    console.log('In another middleware!');
+app.use('/product', (req, res, next)=> {
+    //extract what the user sent
+    console.log(req.body);
+    //redirection
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next) => {
     res.send("<h1>Hello from Express!</h1>")
 })
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
