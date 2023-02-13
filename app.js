9c045e7a06014ec51ab6d078c2ae87a72360b1c2
Collins Kariuki <collinsmunkary@gmail.com>
2023-02-13 04:27:48 +0300

    middlewares and sending back responses

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+//import express
+const express = require('express');
+
+//create express application, initializes a new app
+const app = express();
+
+app.use((req,res,next) => {
+    console.log('In the middleware!');
+    next(); //Allows the request to continue to the next middleware in line
+})
+
+app.use((req,res,next) => {
+    console.log('In another middleware!');
+    res.send("<h1>Hello from Express!</h1>")
+})
+
+const server = http.createServer(app);
+
+server.listen(3000);
