4de95752c46a94aea44cb9cb64dc86032b26d778
Collins Kariuki <collinsmunkary@gmail.com>
2023-03-13 04:39:15 +0300

    installing and configuring pug

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ const shopRoutes = require('./routes/shop.js')
 
 //create express application, initializes a new app
 const app = express();
+
+//LET EXPRESS JS KNOW THAT WE WILL USED A TEMPLATING ENGINE TO RENDER
+//DYNAMIC TEMPLATES - set  global configuration val
+app.set('view engine', 'pug');
+//app.set('view', 'views')
+
 //body parser - done before routing as each request should
 //be parsed no matter where it ends up
 app.use(bodyParser.urlencoded({extended: false}));
