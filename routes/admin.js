83b0d3a669ee63a4cc0f2820fdcfb021edbe7cda
Collins Kariuki <collinsmunkary@gmail.com>
2023-05-22 09:10:57 +0300

    Installing mongodb driver and connecting our app to mongodb

--- routes/admin.js
+++ routes/admin.js
@@ -22,5 +22,3 @@ router.post('/edit-product', adminController.postEditProduct);
 router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
-
-//add some things then remove this line
--- util/database.js
+++ util/database.js
@@ -1,8 +1,19 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+//extract mongo client constructor
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  //connect to the MongoDB database
+  MongoClient.connect('mongodb+srv://collinsmunkary:8ADEBIOKzJBVRPMY@cluster0.kiles44.mongodb.net/?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('Connected!')
+    callback(client);
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
+module.exports = mongoConnect;
+
